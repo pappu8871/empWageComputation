b48107481208ddc3a56ec2817c4b0b898a30f6b6
your username <59005110+pappu8871@users.noreply.github.com>
2021-04-29 13:20:18 +0530

    Added:Totalworking or day is reached for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,19 +3,23 @@
 IS_PRESENT_FULL_TIME=1
 IS_PRESENT_PART_TIME=2
 WAGE_PER_HR=20
-MAX_WORKING_DAY=20
-
-for((counter=1;counter<= $MAX_WORKING_DAY;counter++))
+MAX_WORKING_DAYS=30
+#variable
+totalWorkingHours=0
+totalWorkingDays=0
+while [ $totalWorkingDays != $MAX_WORKING_DAYS ]
 do
 employeeCheck=$((Random%3))
+((totalWorkingDays++))
 case $employeeCheck in
     $IS_PREESENT_FULL_TIME)
 empHrs=8;;
 $IS_PRESENT_PART_TIME)
-  empHrs=4;;
+empHrs=4;;
 *)
 empHrs=0;;
 esac
-salary=$(($empHrs * $WAGE_PER_HR))
+  totalWorkingHours=$(($totalWorkingHours + $empHrs ))
+    #salary=$(($empHrs * $WAGE_PER_HR))
 done
-
+  salary=$(( totalworkingHours * $WAGE_PER_HR ))
