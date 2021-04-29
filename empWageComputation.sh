d8db81806ee7d19dc1bf19778067390428ed1b1f
your username <59005110+pappu8871@users.noreply.github.com>
2021-04-29 11:42:52 +0530

    Addea: Employee Attendence

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo "Welcome to employee"
+IS_PRESENT=1
+#Variables
+empCheck=$(( RANDOM%2 ))
+
+#Selection
+if [ $empCheck -eq $IS_PRESENT ]
+then
+echo "Employee is present"
+else
+   echo "Employee is absent"
+fi
+
