846db1bc083821d1e64bdc80b03d4ec7bfc82969
your username <59005110+pappu8871@users.noreply.github.com>
2021-04-29 13:28:16 +0530

    Added:Write a function get work hour

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,24 +2,35 @@
 
 IS_PRESENT_FULL_TIME=1
 IS_PRESENT_PART_TIME=2
-WAGE_PER_HR=20
-MAX_WORKING_DAYS=30
+EMP_RATE_PER_HR=20
+NO_OF_WORKING_DAYS=5
+MAX_WORKING_DAYS=20
 #variable
 totalWorkingHours=0
 totalWorkingDays=0
-while [ $totalWorkingDays != $MAX_WORKING_DAYS ]
-do
-employeeCheck=$((Random%3))
-((totalWorkingDays++))
+
+function getWorkingHours(){
+local empCheck=$1
+localempHrs=0
 case $employeeCheck in
-    $IS_PREESENT_FULL_TIME)
-empHrs=8;;
-$IS_PRESENT_PART_TIME)
-empHrs=4;;
-*)
-empHrs=0;;
+
+    $IS_PREESENT_FULL_TIME ) empHrs=8;;
+
+       $IS_PRESENT_PART_TIME ) empHrs=4;;
+       *) empHrs=0;;
 esac
-  totalWorkingHours=$(($totalWorkingHours + $empHrs ))
-    #salary=$(($empHrs * $WAGE_PER_HR))
+  echo $empHrs
+   }
+
+
+while [ $totalWorkingDays -lt $NO_OF_WORKING_DAYS ] && [ $totalWorkingHours -lt>
+
+do
+employeeCheck=$(( Random%3 ))
+totalWorkingDays=$(( $totalWorkingDays + 1 ))
+empHrs="$( getWorkingHours $empCheck )"
+
+  totalWorkingHours=$(( $totalWorkingHours + $empHrs ))
+
 done
-  salary=$(( totalworkingHours * $WAGE_PER_HR ))
+  salary=$(( $EMP_RATE_PER_HR * $totalWorkingHours  ))
