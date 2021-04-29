58ca62fa646118302b0a7fbebe59ad51925d68f7
your username <59005110+pappu8871@users.noreply.github.com>
2021-04-29 16:38:44 +0530

    Added: the result in a Dictionary for every Computation

--- arithComputation.sh
+++ arithComputation.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+echo "Please enter your first number: "
+read a
+echo "Enter second number: "
+read b
+echo "Enter third number: "
+read c
+
+ declear -A arrayName
+
+p=$(( a + b * c ))
+echo "p:$p"
+
+s=$(( a * b + c ))
+echo "s:$s"
+
+
+
+q=$(( c + a / b ))
+echo "q:$q"
+
+
 
 t=$(( a % b + c ))
 echo "t:$t"
