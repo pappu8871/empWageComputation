b5004b6d1b84f7ef4bc29e28e0fa5076325da380
your username <59005110+pappu8871@users.noreply.github.com>
2021-04-29 12:55:53 +0530

    Added:Wage for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,7 +3,10 @@
 IS_PRESENT_FULL_TIME=1
 IS_PRESENT_PART_TIME=2
 WAGE_PER_HR=20
+MAX_WORKING_DAY=20
 
+for((counter=1;counter<= $MAX_WORKING_DAY;counter++))
+do
 employeeCheck=$((Random%3))
 case $employeeCheck in
     $IS_PREESENT_FULL_TIME)
@@ -15,3 +18,4 @@ empHrs=0;;
 esac
 salary=$(($empHrs * $WAGE_PER_HR))
 done
+
