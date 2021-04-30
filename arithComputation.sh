15873c21de15674e2da1e52083789f8f8b7de15e
your username <59005110+pappu8871@users.noreply.github.com>
2021-04-30 10:16:53 +0530

    added: Show Computation value in ascending order

--- arithComputation.sh
+++ arithComputation.sh
@@ -23,3 +23,24 @@ echo "s:$s"
 arrayName=( $p $q $r $s )
 
 echo ${arrayName[@]}
+
+  
+ 
+for ((i = 0; i<5; i++))
+do
+      
+    for((j = 0; j<5-i-1; j++))
+    do
+      
+        if [ ${arrayName[j]} -gt ${arrayName[$((j+1))]} ]
+        then
+            # swap
+            temp=${arrayName[j]}
+            arrayName[$j]=${arrayName[$((j+1))]}  
+            arrayName[$((j+1))]=$temp
+        fi
+    done
+done
+  
+echo "Array in sorted order :"
+echo ${arrayName[@]}
